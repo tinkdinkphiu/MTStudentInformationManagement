ee9ee7f137079550b9b0238316db686832a9a214
letranphu <52100572@student.tdtu.edu.vn>
2023-11-18 08:48:56 +0700

    feat: create,edit student gender, major, date restricted

--- app/src/main/java/com/phule/mtstudentinformationmanagement/CreateStudentActivity.java
+++ app/src/main/java/com/phule/mtstudentinformationmanagement/CreateStudentActivity.java
@@ -32,42 +32,6 @@ public class CreateStudentActivity extends AppCompatActivity {
 
         initUi();
         initListener();
-        etBirthday = findViewById(R.id.et_birthday);
-        etBirthday.setShowSoftInputOnFocus(false);
-        etBirthday.setCursorVisible(false);
-        etBirthday.setFocusableInTouchMode(true);
-
-        etBirthday.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openDialog(etBirthday);
-            }
-        });
-
-        etEnrollmentDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openDialog(etEnrollmentDate);
-            }
-        });
-    }
-
-    public void openDialog(final EditText editText) {
-        Calendar calendar = Calendar.getInstance();
-        DatePickerDialog dialog = new DatePickerDialog(this, R.style.MyDatePickerDialogTheme, new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                editText.setText(String.format("%d/%d/%d", day, month, year));
-            }
-        }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DATE));
-        dialog.setOnShowListener( new DialogInterface.OnShowListener() {
-            @Override
-            public void onShow(DialogInterface arg0) {
-                dialog.getButton(AlertDialog.BUTTON_NEGATIVE).setTextColor(ContextCompat.getColor(CreateStudentActivity.this, R.color.green_primary));
-                dialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(ContextCompat.getColor(CreateStudentActivity.this, R.color.green_primary));
-            }
-        });
-        dialog.show();
     }
     private void initUi() {
         ilCode = findViewById(R.id.il_code);
@@ -87,6 +51,9 @@ public class CreateStudentActivity extends AppCompatActivity {
         etPhone = findViewById(R.id.et_phone);
         etEnrollmentDate = findViewById(R.id.et_enrollment_date);
         etMajor = findViewById(R.id.et_major);
+
+        etBirthday.setShowSoftInputOnFocus(false);
+        etEnrollmentDate.setShowSoftInputOnFocus(false);
 
         btnCreate = findViewById(R.id.btn_create);
     }
@@ -117,5 +84,128 @@ public class CreateStudentActivity extends AppCompatActivity {
                         });
             }
         });
+        etBirthday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDatePickerDialog(etBirthday);
+            }
+        });
+        etEnrollmentDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDatePickerDialog(etEnrollmentDate);
+            }
+        });
+        etGender.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGenderDialog(etGender);
+            }
+        });
+        etMajor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMajorDialog(etMajor);
+            }
+        });
+    }
+
+    private void openDatePickerDialog(final EditText editText) {
+        Calendar calendar = Calendar.getInstance();
+        DatePickerDialog dialog = new DatePickerDialog(this, R.style.MyDatePickerDialogTheme, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                editText.setText(String.format("%d/%d/%d", day, month, year));
+            }
+        }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DATE));
+
+        // Set the maximum date to the current date
+        dialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+
+        dialog.setOnShowListener( new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface arg0) {
+                dialog.getButton(AlertDialog.BUTTON_NEGATIVE).setTextColor(ContextCompat.getColor(CreateStudentActivity.this, R.color.green_primary));
+                dialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(ContextCompat.getColor(CreateStudentActivity.this, R.color.green_primary));
+            }
+        });
+        dialog.show();
+    }
+
+    private void openGenderDialog(final TextInputEditText editText) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Select Gender");
+        final String[] genderOptions = {"Male", "Female"};
+        builder.setItems(genderOptions, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                editText.setText(genderOptions[which]);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
+    }
+    private void openMajorDialog(final TextInputEditText editText) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Select Major");
+        final String[] majorOptions = {
+                "Accounting",
+                "Applied Mathematics",
+                "Architecture",
+                "Biotechnology",
+                "Business Administration",
+                "Business Administration - Major in Marketing Management",
+                "Business Administration - Major in Restaurant Management",
+                "Chemical Engineering",
+                "Chinese - Major in Chinese",
+                "Chinese - Major in Chinese-English",
+                "Civil Engineering",
+                "Computer Network and Data Communication",
+                "Computer Science",
+                "Control and Automation Engineering",
+                "Electronic - Communication Engineering",
+                "Electrical Engineering",
+                "English",
+                "Environmental Science",
+                "Environmental Technology",
+                "Fashion Design",
+                "Finance - Banking",
+                "Graphic Design",
+                "Industrial Design",
+                "Interior Design",
+                "International Business",
+                "Labor Protection",
+                "Labor Relations",
+                "Law",
+                "Pharmacy",
+                "Social Work",
+                "Software Engineering",
+                "Sociology",
+                "Sports Management - Major in Sports Business and Event Organization",
+                "Statistics",
+                "Transportation Engineering",
+                "Urban Planning",
+                "Vietnamese Studies - Major in Tourism and Tourism Management",
+                "Vietnamese Studies - Major in Tourism and Travel",
+                "Vietnamese Studies - Major in Vietnamese Language"
+        };
+        builder.setItems(majorOptions, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                editText.setText(majorOptions[which]);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
     }
 }
