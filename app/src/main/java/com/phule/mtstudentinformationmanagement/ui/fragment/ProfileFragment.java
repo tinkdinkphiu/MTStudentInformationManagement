a678b14b4ed352a5f3060650990cae5c30127d97
letranphu <52100572@student.tdtu.edu.vn>
2023-11-29 16:37:50 +0700

    feat: change profile photo

--- app/src/main/java/com/phule/mtstudentinformationmanagement/ui/fragment/ProfileFragment.java
+++ app/src/main/java/com/phule/mtstudentinformationmanagement/ui/fragment/ProfileFragment.java
@@ -2,68 +2,63 @@ package com.phule.mtstudentinformationmanagement.ui.fragment;
 
 import static android.app.Activity.RESULT_OK;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
+import android.Manifest;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
 import com.github.dhaval2404.imagepicker.ImagePicker;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.phule.mtstudentinformationmanagement.R;
 import com.phule.mtstudentinformationmanagement.data.model.User;
 import com.phule.mtstudentinformationmanagement.helper.DialogHelper;
+import com.squareup.picasso.Picasso;
 
 import kotlin.Unit;
 import kotlin.jvm.functions.Function1;
 /*import com.phule.mtstudentinformationmanagement.ui.activity.ChangeAvatarActivity;*/
 
 public class ProfileFragment extends Fragment {
-    private final int GALLERY_REQ_CODE = 1000;
+    private final int GALLERY_REQUEST_CODE = 1000;
     private FirebaseFirestore firebaseFirestore;
-    private FirebaseUser user;
+    private FirebaseUser firebaseUser;
     private TextInputLayout ilEmail, ilName, ilAge, ilPhone, ilStatus, ilRole;
     private TextInputEditText etEmail, etName, etAge, etPhone, etStatus, etRole;
     private ImageView btnAvatar;
     private Button btnDone;
     private String originalEmail;
     private DialogHelper dialogHelper;
-    ActivityResultLauncher<Intent>  imagePickLaucher;
-    Uri selectedImageUri;
-    User currentUser;
-    ProgressBar progressBar;
 
-    public ProfileFragment(){
+    public ProfileFragment() {
 
-    }
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        imagePickLaucher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
-                result -> {
-                    if(result.getResultCode() == RESULT_OK){
-                        Intent data = result.getData();
-                        if(data != null && data.getData() != null){
-                            selectedImageUri = data.getData();
-                            DialogHelper.setProfilePic(getContext(), selectedImageUri, btnAvatar);
-                        }
-                    }
-                }
-                );
     }
 
     @Override
@@ -75,33 +70,10 @@ public class ProfileFragment extends Fragment {
         dialogHelper = new DialogHelper(requireContext());
         initialFirebase();
         initUi(view);
-        getUserData();
 
-        btnAvatar.setOnClickListener((view1 -> {
-            ImagePicker.with(this).cropSquare().compress(512).maxResultSize(512, 512)
-                    .createIntent(new Function1<Intent, Unit>() {
-                        @Override
-                        public Unit invoke(Intent intent) {
-                            imagePickLaucher.launch(intent);
-                            return null;
-                        }
-                    });
-        }));
+        initListener();
 
-        btnDone.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(selectedImageUri != null){
-                    DialogHelper.getCurrentProfilePicStorageRef().putFile(selectedImageUri)
-                            .addOnCompleteListener(task -> {
-                               updateToFirestore();
-                            });
-                    Toast.makeText(getContext(), "Your avatar has been saved", Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(getContext(), "Error", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+        getUserProfileImage(btnAvatar);
 
         return view;
     }
@@ -109,8 +81,19 @@ public class ProfileFragment extends Fragment {
 
     private void initialFirebase() {
         firebaseFirestore = FirebaseFirestore.getInstance();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
     }
-    
+
+    private void initListener() {
+        btnAvatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, GALLERY_REQUEST_CODE);
+            }
+        });
+    }
+
     private void initUi(View view) {
         ilEmail = view.findViewById(R.id.il_email);
         ilName = view.findViewById(R.id.il_name);
@@ -129,6 +112,7 @@ public class ProfileFragment extends Fragment {
         btnAvatar = view.findViewById(R.id.btnAvatar);
         btnDone = view.findViewById(R.id.btnDone);
     }
+
     private void populateField(String email, String name, String age, String phone, String status, String role) {
         etEmail.setText(email);
         etName.setText(name);
@@ -137,43 +121,62 @@ public class ProfileFragment extends Fragment {
         etStatus.setText(status);
         etRole.setText(role);
     }
-    private void updateToFirestore() {
-        if (currentUser != null) {
-            DialogHelper.currentUserDetails().set(currentUser)
-                    .addOnCompleteListener(task -> {
-                        setInProgress(false);
-                        if (task.isSuccessful()) {
-                            Toast.makeText(getContext(), "Thành công", Toast.LENGTH_SHORT).show();
-                        } else {
-                            Toast.makeText(getContext(), "Thất bại", Toast.LENGTH_SHORT).show();
-                            if (task.getException() != null) {
-                                Log.e("Firestore Update", "Error: " + task.getException().getMessage());
-                            }
-                        }
-                    });
-        } else {
-            Log.e("Firestore Update", "Error: currentUser is null");
-        }
-    }
-    void setInProgress(boolean inProgress){
-        if(inProgress){
-            progressBar.setVisibility(View.VISIBLE);
-            btnDone.setVisibility(View.GONE);
-        }else{
-            progressBar.setVisibility(View.GONE);
-            btnDone.setVisibility(View.VISIBLE);
-        }
-    }
 
-    private void getUserData() {
-        DialogHelper.getCurrentProfilePicStorageRef().getDownloadUrl()
-                .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        Uri uri = task.getResult();
-                        Glide.with(getContext())
-                                .load(uri)
-                                .into(btnAvatar);
+    public void getUserProfileImage(ImageView imageView) {
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference("images/" + firebaseUser.getUid() + "/profile_picture.jpg");
+        storageReference.getDownloadUrl()
+                .addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        // The download URL of the image
+                        String imageUrl = uri.toString();
+
+                        // Load the image into the CircleImageView
+                        Picasso.get().load(imageUrl).into(imageView);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        Toast.makeText(getActivity(), exception.getMessage(), Toast.LENGTH_SHORT).show();
                     }
                 });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY_REQUEST_CODE && resultCode == Activity.RESULT_OK && data != null && data.getData() != null) {
+            Uri selectedImageUri = data.getData();
+
+            if (firebaseUser != null) {
+                String userId = firebaseUser.getUid();
+
+                StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+
+                StorageReference fileRef = storageRef.child("images/" + userId + "/profile_picture.jpg");
+
+                fileRef.putFile(selectedImageUri)
+                        .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                // Get the download URL of the uploaded file
+                                fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                    @Override
+                                    public void onSuccess(Uri uri) {
+                                        Glide.with(getContext()).load(uri).into(btnAvatar);
+                                    }
+                                });
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(getContext(), "Failed to upload image", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        }
     }
 }
